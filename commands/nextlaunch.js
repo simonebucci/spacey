33c63bc8a8b092a87f297884f24bd5e337a93da7
Simone <simonebucci@users.noreply.github.com>
2020-06-07 01:54:38 +0200

    Update nextlaunch.js

--- commands/nextlaunch.js
+++ commands/nextlaunch.js
@@ -7,12 +7,62 @@ exports.run = async (client, message, args, level) => { // eslint-disable-line n
   let description = link.details
   let date = link.launch_date_local
   let patch = link.links.mission_patch_small
-
+  let n = link.flight_number
+  let ls = link.launch_site.site_name_long
+  let load = link.rocket.second_stage.payloads[0].payload_type
+  let loadid = link.rocket.second_stage.payloads[0].payload_id
+  let rocket = link.rocket.rocket_name
+/*
   message.channel.send(mission);
   message.channel.send({files: [patch]});
   message.channel.send(description);
   message.channel.send(date);
+*/
 
+  message.channel.send({embed: {
+      color: 3447003,
+      author: {
+        name: "Next SpaceX Launch",
+        icon_url: patch
+      },
+      "thumbnail": {
+      "url": patch
+    },
+      title: mission,
+      url: "http://google.com",
+      description: description,
+      fields: [{
+          name: "Launch Date",
+          value: date
+        },
+        {
+          name: "Rocket",
+          value: `${rocket} `
+        },
+        {
+          name: "Payload",
+          value: `${loadid} `
+        },
+        {
+          name: "Payload Type",
+          value: `${load} `
+        },
+        {
+          name: "Launch site",
+          value: `${ls} `
+        },
+        {
+          name: "Flight Number",
+          value: n
+        }
+      ],
+      timestamp: new Date(),
+      footer: {
+        icon_url: client.user.avatarURL,
+        text: "© SpaceX API"
+      }
+    }
+  });
 
   };
 
