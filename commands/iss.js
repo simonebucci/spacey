1ef7e87cdc33a14d25743704021cfdbfbc6f64b9
Simone <simonebucci@users.noreply.github.com>
2020-06-12 12:43:41 +0200

    Create iss.js

---
+++ commands/iss.js
@@ -0,0 +1,65 @@
+exports.run = async (client, message, args, level) => {
+
+  const fetch = require('node-fetch');
+
+  var link = await fetch('https://api.wheretheiss.at/v1/satellites/25544').then(response => response.json());
+  let latitude = link.latitude
+  let longitude = link.longitude
+  let altitude = link.altitude
+  let velocity = link.velocity
+
+
+  message.channel.send({embed: {
+      color: 3447003,
+      author: {
+        name: "ISS Info",
+        icon_url: 'https://upload.wikimedia.org/wikipedia/commons/c/cf/InternationalSpaceStationPatch.png'
+      },
+      thumbnail: {
+      url:
+    },
+      title: 'ISS info',
+      url: "https://isstracker.spaceflight.esa.int/",
+      description: ,
+      fields: [{
+          name: "Latitude",
+          value: latitude
+        },
+        {
+          name: "Longitude",
+          value: `longitude `
+        },
+        {
+          name: "Altitude",
+          value: `${altitude}Km `
+        },
+        {
+          name: "Velocity",
+          value: `${velocity}Km/h `
+        }
+      ],
+      timestamp: new Date(),
+      footer: {
+        icon_url: client.user.avatarURL,
+        text: "© SpaceY"
+      }
+    }
+  });
+
+
+
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: "User"
+};
+
+exports.help = {
+  name: "iss",
+  category: "Miscelaneous",
+  description: "Info about the International Space Station",
+  usage: "iss"
+};
