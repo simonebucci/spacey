3003cd4d4c71a24fde3b4b84a1c714b602d622be
Simone <simonebucci@users.noreply.github.com>
2020-07-09 16:18:33 +0200

    launch destination added

--- commands/launch.js
+++ commands/launch.js
@@ -3,19 +3,42 @@ exports.run = async (client, message, args, level) => {
   const { promisify } = require('util')
   const sleep = promisify(setTimeout)
 
+  let text = args.join(" ");
   var value = [0,6,9,12];
   const randomValue = Math.floor(Math.random() * value.length);
   var v = value[randomValue];
 
   let score = client.getScore.get(message.author.id, message.guild.id);
-  if(score.money >= 1000){
-    score.money = score.money - 1000;
-    message.channel.send("You paid 1000$ to launch one rocket");
+
+//rocket destinations [name, price, pic, points]
+  var viss = ['the ISS',1000,'https://i.imgur.com/1A9thnP.jpg',100];
+  var vmoon = ['the Moon',10000,'https://www.nasa.gov/images/content/62043main_Footprint_on_moon.jpg',1000];
+  var vmars = ['Mars',100000,'https://www.nasa.gov/sites/default/files/thumbnails/image/curiosityrover_buckskinpanorama_pia19808.jpg',10000];
+
+  switch(text){
+    case 'iss':
+      var vet = viss;
+    break;
+    case 'moon':
+      var vet = vmoon;
+    break;
+    case 'mars':
+      var vet = vmars;
+    break;
+    default:
+      message.channel.send("Please choose a destination for your rocket! (iss, moon, mars) ");
+      var error = 1;
+  }
+
+if(error != 1){
+  if(score.money >= vet[1]){
+    score.money = score.money - vet[1];
+    message.channel.send(`You paid ${vet[1]} to launch one rocket to ${vet[0]}`);
 
     const msg = await message.channel.send("Launch Countdown");
     var i;
     const countDown = async () => {
-      for (i = 10; i >= 0;i--) {
+      for (i = 5; i >= 0;i--) {
         await sleep(1000)
         msg.edit(i);
       }
@@ -25,10 +48,10 @@ exports.run = async (client, message, args, level) => {
       msg.edit("Lift Off");
       await sleep(5000)
       if(v !== 0) {
-        message.channel.send({files: ["https://i.imgur.com/1A9thnP.jpg"]});
+        message.channel.send({files: [vet[2]]});
         message.channel.send("Successful Launch!");
-        score.scores = score.scores + 100;
-        message.reply(`You've earned 100 points!`);
+        score.scores = score.scores + vet[3];
+        message.reply(`You've earned ${vet[3]} points!`);
       }
       else{
         message.channel.send({files: ["https://i.imgur.com/COHyS7l.jpg"]});
@@ -38,9 +61,15 @@ exports.run = async (client, message, args, level) => {
 
     countDown()
   }else{
-    message.channel.send("You don't have enough money to launch a rocket!");
+    message.channel.send(`You don't have enough money to launch a rocket to ${vet[0]}`);
   }
     client.setScore.run(score);
+}else{
+  error = 0;
+}
+
+
+
 };
 
 exports.conf = {
@@ -53,6 +82,6 @@ exports.conf = {
 exports.help = {
   name: "launch",
   category: "Miscelaneous",
-  description: "launch a rocket!",
+  description: "launch a rocket! (iss, moon, mars)",
   usage: "launch"
 };
