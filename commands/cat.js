91e6781fb3684c00571c9c5ff7db8583b5ce9a5e
Simone <simonebucci@users.noreply.github.com>
2020-07-16 21:22:07 +0200

    Update cat.js

--- commands/cat.js
+++ commands/cat.js
@@ -1,10 +1,12 @@
 exports.run = async (client, message, args, level) => { // eslint-disable-line no-unused-vars
   const fetch = require('node-fetch');
 
-  const { file } = await fetch('https://aws.random.cat/meow').then(response => response.json());
-  message.channel.send(file);
+  var link = await fetch('https://api.thecatapi.com/v1/images/search').then(response => response.json());
+  let pic = link[0].url
+  message.channel.send(pic);
 
     };
+
 
   exports.conf = {
     enabled: true,
